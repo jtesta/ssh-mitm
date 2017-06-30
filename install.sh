460d4e430f8abe84eaf048331f7339e4b94ba925
Joe Testa <jtesta@positronsecurity.com>
2017-06-30 16:15:47 -0400

    Added GPLv3 header.

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# install.sh
+# Copyright (C) 2017  Joe Testa <jtesta@positronsecurity.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 openssh_sources='openssh-7.5p1.tar.gz'
 openssh_source_dir='openssh-7.5p1'
 mitm_patch='openssh-7.5p1-mitm.patch'
