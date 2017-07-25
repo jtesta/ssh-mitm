05b46f7b318fbbdae967792e18693d16cc7d0f49
Joe Testa <jtesta@positronsecurity.com>
2017-07-24 21:08:26 -0400

    Added debug_run.sh for development.

---
+++ debug_run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script is used during the development process to compile and re-deploy
+# sshd_mitm.
+
+killall -u ssh-mitm
+
+pushd openssh-7.5p1-mitm
+if [[ $1 == 'clean' ]]; then
+   make clean
+   make -j 10 > /dev/null
+else
+   make
+fi
+popd
+
+if [[ (! -f openssh-7.5p1-mitm/sshd) || (! -f openssh-7.5p1-mitm/ssh) || (! -f openssh-7.5p1-mitm/sftp-server) ]]; then
+   echo -e "\nCompile error."
+   exit -1
+fi
+
+cp openssh-7.5p1-mitm/sshd ~ssh-mitm/bin/sshd_mitm
+cp openssh-7.5p1-mitm/ssh ~ssh-mitm/bin/ssh
+cp openssh-7.5p1-mitm/sftp-server ~ssh-mitm/bin/sftp-server
+su - ssh-mitm -c "./run.sh"
