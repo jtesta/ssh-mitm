43418d1d554f199b8114f6cd70564923485a7446
Joe Testa <jtesta@positronsecurity.com>
2018-04-21 16:44:11 -0400

    Clarified licensing of code.

--- dev/redeploy.sh
+++ dev/redeploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-
+#
+# Copyright (C) 2017-2018  Joe Testa <jtesta@positronsecurity.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
 # This script is used during the development process to compile and re-deploy
 # sshd_mitm.
 
