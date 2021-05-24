d46377bed1235595affd354a65b211a328afcfcc
Timothy Brush <8075964+timothybrush@users.noreply.github.com>
2021-05-24 06:06:18 -0700

    Moved client.log and session files under /home/ssh-mitm/log. (#31)
    Changed ownership of all files under /home/ssh-mitm to ssh-mitm:ssh-mitm.

--- install.sh
+++ install.sh
@@ -218,7 +218,7 @@ function setup_environment {
     # respectively.
     useradd -m -s /bin/bash ssh-mitm
     chmod 0700 ~ssh-mitm
-    mkdir -m 0755 ~ssh-mitm/{bin,etc}
+    mkdir -m 0755 ~ssh-mitm/{bin,etc,log}
     mkdir -m 0700 ~ssh-mitm/tmp
     chown ssh-mitm:ssh-mitm ~ssh-mitm/tmp
 
@@ -245,9 +245,6 @@ function setup_environment {
     # to not be created properly at run-time...).
     mkdir -m 0700 ~ssh-mitm/empty ~ssh-mitm/.ssh
 
-    # Set ownership on the "empty" directory and SSH host keys.
-    chown ssh-mitm:ssh-mitm /home/ssh-mitm/empty /home/ssh-mitm/.ssh /home/ssh-mitm/etc/ssh_host_*key*
-
     # Create the "run.sh" script, then set its permissions.
     cat > ~ssh-mitm/run.sh <<EOF
 #!/bin/bash
@@ -261,6 +258,9 @@ else
 fi
 EOF
     chmod 0755 ~ssh-mitm/run.sh
+
+    # Set ownership of files under /home/ssh-mitm
+    chown -R ssh-mitm:ssh-mitm /home/ssh-mitm
 
     # Install the AppArmor profiles.
     if [[ ! -d /etc/apparmor.d ]]; then
