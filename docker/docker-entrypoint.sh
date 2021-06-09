58ff4505b135dd5be8ebc0e36e554d73898620d8
Joe Testa <jtesta@positronsecurity.com>
2021-06-08 22:14:31 -0400

    Now prints host key fingerprints on startup.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 if [ ! -f /home/ssh-mitm/etc/ssh_host_rsa_key ]; then
-    /usr/bin/ssh-keygen -q -t rsa -b 4096 -f /home/ssh-mitm/etc/ssh_host_rsa_key -N ''
-    /usr/bin/ssh-keygen -q -t ed25519 -f /home/ssh-mitm/etc/ssh_host_ed25519_key -N ''
+    echo -e "\nSSH host keys not found.  Generating new ones...\n"
+    /usr/bin/ssh-keygen -t rsa -b 4096 -f /home/ssh-mitm/etc/ssh_host_rsa_key -N ''
+    /usr/bin/ssh-keygen -t ed25519 -f /home/ssh-mitm/etc/ssh_host_ed25519_key -N ''
+    echo
+else
+    echo -e "\nExisting SSH host keys found:\n"
+    /usr/bin/ssh-keygen -lf /home/ssh-mitm/etc/ssh_host_rsa_key
+    /usr/bin/ssh-keygen -lf /home/ssh-mitm/etc/ssh_host_ed25519_key
 fi
 
 # start default cmd
